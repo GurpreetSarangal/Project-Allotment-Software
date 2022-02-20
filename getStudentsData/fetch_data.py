2ce104ecb95bf6af3a538d43b7648594cad4ef7d
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2022-02-20 20:27:04 +0530

    Separated: logging in & inputting & fetching logic

--- getStudentsData/fetch_data.py
+++ getStudentsData/fetch_data.py
@@ -1,13 +1,36 @@
+from operator import index
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
+import pandas as pd
 # import os
 import time
 
 
-driver = webdriver.Chrome()
-driver.get('http://127.0.0.1:5500/test2.html')
 
+
+def fetch_students(driver):
+    table = driver.find_element(By.XPATH, '//table[@id="gridViewDetail"]')
+    header = []
+    data = []
+    for row in table.find_elements(By.XPATH,'.//tr/th'):
+        # print(row.text)
+        header.append(row.text)
+    del header[0]
+
+    for row in table.find_elements(By.XPATH,'.//tr'):
+        # print(row.text)
+        st_det = []
+        for td in row.find_elements(By.XPATH, './/td'):
+            # print(td.text)
+            st_det.append(td.text)
+        if len(st_det) > 0:
+            st_det.remove('')
+            data.append(st_det)
+
+    driver.close()
+    csv_data = pd.DataFrame(data, columns=header)
+    csv_data.to_csv(f'data/student/{(data[0])[2]}-{(data[0])[3]}.csv', index=False)
