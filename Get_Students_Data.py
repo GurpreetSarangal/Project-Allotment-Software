b9d820bb479abf045e2895e3cb87f6f7a61a4f5c
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2022-02-21 21:36:42 +0530

    feature: csv to database

--- Get_Students_Data.py
+++ Get_Students_Data.py
@@ -2,18 +2,21 @@ from getStudentsData.fetch_data import *
 from getStudentsData.inputs import *
 from getStudentsData.login_script import *
 from getStudentsData.select_class import *
-
+from getStudentsData.format_data import *
+from getStudentsData.csv_to_db import *
 # data = get_inputs()
 data = [
     "Dec-2021",
     "29-01-2022",
     "A",
-    15,
-    "Bachelor of Computer Applications",
+    14,
+    "B.Sc. (Information Technology)",
     5,
     "computer Networks",
     2021,
 ]
 driver = login()
 set_options(driver, data)
-fetch_students(driver)
+raw_csv_path = fetch_students(driver)
+csv_path = format_csv(raw_csv_path)
+insert_stu_details(csv_path)
