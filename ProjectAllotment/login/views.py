fba8fc60d2486c4d16e4d29ed754fe136a374c18
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2022-03-07 18:55:24 +0530

    Added: login as staff and superuser

--- ProjectAllotment/login/views.py
+++ ProjectAllotment/login/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render, HttpResponse
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+def index(request):
+    # TODO: add a landing page
+    return redirect('login')
+
 def login_user(request):
-    # if request.method == "POST":
-    return HttpResponse("this is login")
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            form = login(request, user)
+            if request.user.is_superuser:
+                return redirect('adminDashboard')
+            return redirect('home')
+        else:
+            messages.error(request, "wrong credentials")
+            return redirect('login')
+    
+    
+    return render(request, 'login/login_form.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
--- ProjectAllotment/staff/views.py
+++ ProjectAllotment/staff/views.py
@@ -3,17 +3,11 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def dashboard(request, ):
-    context={
-        "username":f"Welcome staff",
-    }
+def dashboard(request):
     return render(request, 'staff_dashboard.html')
 
 def allocate_project(request):
-    context={
-        "heading":f"Project Allocation Window",
-    }
-    return render(request, 'allocate_project.html',context)
+    return render(request, 'allocate_project.html')
 
 def allocate_guides_groups(request):
     return HttpResponse("Allocate- guides and groups here")
